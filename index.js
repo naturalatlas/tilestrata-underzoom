1b0e3006aadc6de921a67c7b566095c24c6a4d99
Brandon Reavis <brandon@naturalatlas.com>
2018-03-06 12:29:20 -0800

    Ignore 403 errors and treat as transparent

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ module.exports = function(options) {
 						childReq.y = coords[1];
 						childReq.z = coords[2];
 						options.source.serve(server, childReq, function(err, childBuffer, childHeaders) {
-							if (err && err.statusCode !== 404) return callback(err);
-							if (err && err.statusCode === 404) return callback();
+							if (err && (err.statusCode !== 404 && err.statusCode !== 404)) return callback(err);
+							if (err && (err.statusCode === 404 || err.statusCode === 403)) return callback();
 							if (!childBuffer) return callback();
 							Mapnik.Image.fromBytes(childBuffer, function(err, image) {
 								if (err) return callback(err);
